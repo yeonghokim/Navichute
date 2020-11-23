b37395184d76fb46591da42634a88e9af4945d8a
YeonghoKim <saymi1973@naver.com>
2020-11-23 22:47:51 +0900

    Add files via upload

---
+++ Navichute/app/src/main/java/swc/chuma/navichute/MainActivity.java
@@ -0,0 +1,25 @@
+package swc.chuma.navichute;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        //mapView.setMapViewEventListener(this); // this에 MapView.MapViewEventListener 구현.
+        //mapView.setPOIItemEventListener(this);
+    }
+}
