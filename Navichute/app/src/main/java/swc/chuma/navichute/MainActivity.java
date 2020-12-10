4e47c3ff281bec31b444acbdf9be41b634be04c2
YeonghoKim <saymi1973@naver.com>
2020-12-10 23:52:56 +0900

    Add files via upload

--- Navichute/app/src/main/java/swc/chuma/navichute/MainActivity.java
+++ Navichute/app/src/main/java/swc/chuma/navichute/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 
@@ -17,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
         MapView mapView = new MapView(this);
         ViewGroup mapViewContainer = findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(7, true);
+
+        // 중심점 변경 + 줌 레벨 변경
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 9, true);
+
+        // 줌 인
+        mapView.zoomIn(true);
+
+        // 줌 아웃
+        mapView.zoomOut(true);
 
         //mapView.setMapViewEventListener(this); // this에 MapView.MapViewEventListener 구현.
         //mapView.setPOIItemEventListener(this);
